8a21c9f3ecb5d91f6a827767e2dcc56128f1fb0d
Adrian Molina <admolina97@gmail.com>
2026-07-29 19:36:10 -0600

    Drag&Drop event added

--- JS/app.js
+++ JS/app.js
@@ -8,6 +8,9 @@ var error; //mensaje de error si se rellena mal el formulario
 var selectDificulty; //dificultad seleccionada por el jugador
 var totalCards;
 var submitForm; 
+var avatar; //Avatar principal seleccionado por el usuario
+var dragabbleItem; //Avatares que puede arrastrar y seleccionar el usuario
+var avatarContainer; //Contenedor del avatar
 
 /**
  * Funciones de evento
@@ -20,7 +23,24 @@ function checkForm(event){
         event.preventDefault();
         error.innerText="Selecciona la cantidad de cartas!"
     }
+    saveUserData();
     return;
+}
+
+function saveUserData(){
+    let userData={
+        "nickname": nickname.value,
+        "avatar": avatar.src,
+    }
+    localStorage.setItem('users', JSON.stringify(userData));
+}
+
+function dragItem(event){
+    item=event.target;
+}
+
+function changeAvatar(){
+    avatar.src=item.src;
 }
 
 /**
@@ -33,11 +53,26 @@ function DOMloaded(){
     submitForm=document.getElementById('form');
     selectDificulty=document.getElementById('dificulty');
     totalCards=document.getElementById('totalcards');
+    avatar=document.getElementById('avatar-selected');
+    dragabbleItem=document.getElementsByClassName('avatar');
+    avatarContainer=document.getElementById('main-avatar');
 
     /**
      * Carga de eventos
      */
     submitForm.addEventListener('submit', checkForm);
+
+    /**
+     * Drag & Drop Event
+     */
+    //DragStart
+    for(let item of dragabbleItem){
+        item.addEventListener('dragstart', dragItem);
+    }
+    //Dragover
+    avatarContainer.addEventListener('dragover', (e)=> {e.preventDefault()});
+    //Drop
+    avatarContainer.addEventListener('drop', changeAvatar);
 }
 
 document.addEventListener('DOMContentLoaded', DOMloaded);
---
+++ JS/game.js
@@ -0,0 +1,5 @@
+/**
+ * Player data display & game functionality
+ */
+
+
