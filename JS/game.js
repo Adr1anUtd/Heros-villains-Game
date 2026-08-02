74243364c3a23ec6a8064ea98c0aa59c24170698
Adrian Molina <admolina97@gmail.com>
2026-08-01 22:54:43 -0600

    display settings added

--- JS/game.js
+++ JS/game.js
@@ -3,17 +3,39 @@
  */
 //variables
 var avatarGame=document.getElementById('avatar-game');
+var displayNick=document.getElementById('game-user');
+var displayDificulty=document.getElementById('game-dificulty');
+var gametries=document.getElementById('game-tries');
 
 //Funciones de juego
 function getUserAvatar(){
-    avatarGame.src= useravatar
+    avatarGame.src= useravatar;
+}
+
+function getUserNick(userNick){
+    displayNick.value= userNick;
+}
+
+function getDificulty(userdificulty){
+    if(userdificulty.value = "1"){
+        displayDificulty.value = "Baja"
+        gametries.value=15;
+    }else if(userdificulty.value = "2"){
+        displayDificulty.value="Media"
+        gametries.value=10;
+    }else{
+        displayDificulty.value="Alta"
+        gametries.value=5;
+    }
+    return;
 }
 
 
 //Obtenemos datos de usuario actual
 getUser();
+//Obtenemos avatar 
 getUserAvatar();
-
-
-
-
+//Obtenemos userNick
+getUserNick(userNick);
+//Obtenemos dificultad elegida por el usuario
+getDificulty(userdificulty);
